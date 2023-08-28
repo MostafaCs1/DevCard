f7c62e60f477f6493152b545aa6afc0efcda52bf
Mostafa Mortezapour <mostafamortezapour1@gmail.com>
2023-08-28 13:39:46 +0330

    Add template to web application

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -2,9 +2,18 @@
 
 namespace DevCard.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
+        [Route("/")]
+        [Route("[action]")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult Contact()
         {
             return View();
         }
